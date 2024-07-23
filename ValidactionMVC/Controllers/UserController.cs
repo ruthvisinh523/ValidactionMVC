a3f14e9d6914fd7e0c583753d260075216426a1c
arjun <arjun@LAPTOP-1B2K66Q1>
2024-07-23 10:07:15 +0530

    Add project files.

---
+++ ValidactionMVC/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidactionMVC.Models;
+
+namespace ValidactionMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel model)
+        {
+            if (ModelState.IsValid)
+            { 
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ ValidactionMVC/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidactionMVC.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [RegularExpression(@"^[a-zA-Z0-9]{5,12}$", ErrorMessage = "Username must be 5-12 characters and contain only letters and numbers")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "Password must be minimum 8 characters, at least one letter and one number")]
+        public string Password { get; set; }
+    }
+}
